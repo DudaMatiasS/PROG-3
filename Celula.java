1ce6380c132ddba6171d9889192c8c7292c1c822
Duda <duda@mobradio.com.br>
2024-07-10 19:48:05 -0300

    Lista SE

---
+++ Celula.java
@@ -0,0 +1,8 @@
+public class Celula{
+    int valor;
+    Celula prox; //referencia
+
+    public Celula(int v){
+        valor=v;
+    }
+}
---
+++ Principal.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Principal {
+    public static void main(String[] args) {
+       ListaSE listaSe = new ListaSE();
+       Celula c1 = new Celula(5);
+       listaSe.inserirNoInicio(c1);
+       c1 = new Celula(10);
+       listaSe.inserirNoFinal(c1);
+       c1 = new Celula(20);
+       listaSe.inserirNoFinal(c1);
+       c1 = new Celula(30);
+       listaSe.inserirNoFinal(c1);
+       c1 = new Celula(40);
+       listaSe.inserirNoFinal(c1);
+       c1 = new Celula(75);
+       listaSe.inserirOrdenadamente(c1);
+       c1 = new Celula(0);
+       listaSe.inserirOrdenadamente(c1);
+       c1 = new Celula(2);
+       listaSe.inserirOrdenadamente(c1);
+       c1 = new Celula(60);
+       listaSe.inserirOrdenadamente(c1);
+       c1 = new Celula(55);
+       listaSe.inserirOrdenadamente(c1);
+       c1 = new Celula(25);
+       listaSe.inserirOrdenadamente(c1);
+
+//
+       listaSe.removerValorEspecifico(5);
+       listaSe.imprimirLista();
+
+
+
+    }
+}
